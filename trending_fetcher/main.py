829279f9374a35cbb514b2b8d22bc6dfa7ed2c74
Kenji <trafalgar1618@hotmail.com>
2020-06-16 22:37:47 +0300

    Finished the trending_fetcher functionality by adding the last file deletion function.

--- trending_fetcher/main.py
+++ trending_fetcher/main.py
@@ -39,8 +39,9 @@ def fetch_trends():
     url = config['default']['trending_url']
     response = twitter.get(url).json()[0]
 
+    trending_amount = int(config['default']['trending_amount'])
     date_created = response["created_at"][:10].replace('-', '_')
-    trends = list(map(extract_trend, response["trends"][:20]))
+    trends = [extract_trend(t) for t in response["trends"][:trending_amount]]
     return (trends, date_created)
 
 
@@ -50,7 +51,19 @@ def save_trends(trends, filename):
         json.dump(trends, f, indent=4)
 
 
+def delete_last_entry():
+    entries = os.listdir(dataset_absdir)
+    files = [e for e in entries if '.json' in e.lower()]
+    files_abspaths = [os.path.join(dataset_absdir, f) for f in files]
+    maximum_trending_entries = int(
+        config['default']['maximum_trending_entries'])
+    if len(files_abspaths) > maximum_trending_entries:
+        last_entry = min(files_abspaths)
+        os.remove(last_entry)
+
+
 if __name__ == "__main__":
     trends, date_created = fetch_trends()
     save_trends(trends, date_created)
+    delete_last_entry()
     pass
